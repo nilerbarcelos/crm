254bccfb18c4353e7cae0ffe8c5374313b7667d3
Diego <diego@diego-laptop.(none)>
2008-10-31 10:13:04 -0200

    Projeto Inicial

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+#   render:text => "<p>DEM vindo</p>"
+  end
+
+end
---
+++ app/models/project.rb
@@ -0,0 +1,11 @@
+class Project < ActiveRecord::Base
+   belongs_to :customer
+
+   named_scope :actives, :conditions => {:active => true} 
+   named_scope :inactives, :conditions => {:active => false}
+
+
+   validates_presence_of :name
+   validates_uniqueness_of :name
+   validates_presence_of :customer_id
+end
