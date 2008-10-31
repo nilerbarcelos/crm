253444e7af7ac883f81659a2fa810231ed08f48e
Diego <diego@diego-laptop.(none)>
2008-10-31 10:37:57 -0200

    2 Versao

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ActiveRecord::Base
   has_many :projects, :dependent => :delete_all
 
-  validates_presence_of :name
+#  validates_presence_of :name
   validates_presence_of :description
   validates_uniqueness_of :name
   validates_format_of :phone, :with => /^(\d|\s)*$/
